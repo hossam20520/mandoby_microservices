9d7b9a2dacd96485613fb8f895c5fc3f6e0d75a9
hossam hassan <hossamhassan889@gmail.com>
2023-04-27 02:30:06 +0200

    fixx

--- product_service/app/main.py
+++ product_service/app/main.py
@@ -7,6 +7,9 @@ from app.categorys.routes import router as categorys_router
 import app.categorys.models as models_categorys 
 import app.units.models as models_units 
 from app.units.routes import router as units_router 
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+
 
 models_units.Base.metadata.create_all(bind=engine) 
 models_categorys.Base.metadata.create_all(bind=engine) 
@@ -15,6 +18,15 @@ models_products.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(categorys_router, tags=["Categorys"], prefix="/api/v1.0/categorys") 
 app.include_router(units_router, tags=["Units"], prefix="/api/v1.0/units") 
 app.include_router(products_router, tags=["Products"], prefix="/api/v1.0/products") 
