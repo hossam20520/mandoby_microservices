2304c604d2d1186a1124387eaacbaf63dab3bff9
hossam hassan <hossamhassan889@gmail.com>
2023-05-02 20:03:33 +0300

    dd

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -54,7 +54,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost" , "http://localhost:8100"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
